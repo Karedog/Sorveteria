94c27d46125684dda0e15b0e653ce9a55ce30f93
Douglas <douglasdanielx@gmail.com>
2025-06-17 09:16:52 -0300

    style: adiciona o responsivo do main pagina 'Home'

--- src/componentes/card_home/card_home.jsx
+++ src/componentes/card_home/card_home.jsx
@@ -2,7 +2,7 @@ import estilo from "./card_home.module.css"
 
 export default function CardHome(props){
     return (
-        <div className={estilo.card_home}>
+        <div className={`${estilo.card_home}`}>
             <h3>{props.titulo}</h3>
             <p className={estilo.textoBold}><span>{props.textoBold}</span></p>
             <p className={estilo.texto}>{props.texto}</p>
